a547adaf481c935f7c0319cbc97a32afa63da257
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-27 23:06:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function findMatching (drivers, name) {
-  drivers.filter(function (driverName) {
+  return drivers.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
